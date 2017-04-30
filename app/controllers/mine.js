18217304dace39a17524d30db5a09caeaecf8676
afbayonac <afbayonac@gmail.com>
2017-04-30 01:58:05 -0500

    :rocket: Reset repo

--- app/controllers/mine.js
+++
@@ -1,9 +0,0 @@
-var elEspectadorMine = require('./elEspectadorMine.js')
-var elTiempoMine = require('./elTiempoMine.js')
-
-var mine = (dateInit) => {
-  elEspectadorMine(dateInit)
-  elTiempoMine()
-}
-
-module.exports = mine
--- app/routes/remateRouter.js
+++
@@ -1,9 +0,0 @@
-const remateRouter = require('express').Router()
-const remate = require('../controllers/remate')
-
-remateRouter.get('/', remate.list)
-remateRouter.post('', remate.create)
-remateRouter.get('/:id', remate.getByID)
-remateRouter.post('/:id', remate.update)
-
-module.exports = remateRouter
